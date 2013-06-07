3b557d269a5df07fce7334b5a9d2c4a2ddf09ecf
Sean Wu <wushuang88@gmail.com>
2013-06-07 12:53:01 +0800

    create a new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blazly::Application.config.secret_token = 'bc3d4e409cab7f6cf9842b3a4d5a81861297fa67effb920c3dc004775d252af007fe1616df399f7efcc37d0fa9706a05389a51c9a1c3f0affbbe402f377404ec'
